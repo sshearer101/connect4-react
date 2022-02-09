f15fe77554644f235b694ecd1654f18753d8509c
Sam Shearer <sshearer101@gmail.com>
2022-02-08 19:26:13 -0700

    first functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react'
 import './App.css';
 import {Connect4} from './components/Connect4'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-        <Connect4 />
+      <h1>Connect Four Game</h1>
+      <h2>With React</h2>
+      <Connect4 />
     </div>
   );
 }
-
-export default App;
--- src/components/Connect4.js
+++ src/components/Connect4.js
@@ -1,54 +1,113 @@
-import { useState } from 'react'
-import { Row } from './Row'
-// import * as gameStyles from '../styles/Home.module.css'
+import React, { useState, useEffect, useRef } from "react";
+ 
+//useRef is a hook that allows you to create reference to DOM element, return mutable object
 
-const gameReducer = (state, action) => { return state}
+const boardSettings = {
+  rows: 8,
+  columns: 10,
+  dropAnimationRate: 50,
+  flashAnimationRate: 600,
+  colors: {
+    empty: "#AAAAAA",
+    p1: "#BB2222",
+    p2: "#2222BB"
+  }
+};
 
-const initialGameState = {
-  player1: 1,
-  player2: 2,
-  currentPlayer: 1,
-  board: [
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-  ],
-  gameOver: false,
-  message: '',
-}
+//Rows and Columns
+//Colors are css
 
-export const Connect4 = () => {
-  const [gameState, setGameState] = useState({
-    gameReducer,
-    initialGameState
-  })
+const winTypes = {
+  vertical: 0,
+  horizontal: 1,
+  forwardsDiagonal: 2,
+  backwardsDiagonal: 3
+};
 
 
-  const play = (c) => {}
-
-  return (
-    <>
-      {/* <Button
-        colorScheme="purple"
-        className={gameStyles.button}
-      >
-        New Game
-      </Button> */}
 
 
-      <table>
-        <tbody>
-          {gameState.board.map((row, i) => (
+export default function Connect4(){
+  const [board, setBoard] = useState(createBoard());
 
-            <Row key={i} row={row} play={play} />
-          ))}
-        </tbody>
-      </table>
+//Setting state to board, which calls createBoard function 
 
-      {/* <Text>{gameState.message}</Text> */}
-    </>
+  const [currentPlayer, setCurrentPlayer] = useState(getFirstPlayerTurn());
+
+  //Setting who goes first in a new game, and calls that function
+
+  const [win, setWin] = useState(null);
+
+  //setting initial win state to null
+
+  const [flashTimer, setFlashTimer] = useState(null);
+  const [dropping, setDropping] = useState(false);
+  const domBoard = useRef(null);
+
+
+  function getIndex(row, column) {
+    const index = row * boardSettings.columns + column;
+    if (index > boardSettings.rows * boardSettings.columns) return null;
+    return index;
+  }
+
+//puts in rows and columns based on parameters that when multiplied by column, creates index values
+//based on input, returns random index for each box < 80 (maybe...)
+
+  function createBoard() {
+    return new Array(boardSettings.rows * boardSettings.columns).fill(
+      boardSettings.colors.empty
+    );
+  }
+
+  //returning array of board based on rows columns and corresponding colors
+
+  function getRowAndColumn(index) {
+    if (index > boardSettings.rows * boardSettings.columns) return null;
+    const row = Math.floor(index / boardSettings.columns);
+    const column = Math.floor(index % boardSettings.columns);
+    return {
+      row,
+      column
+    };
+  }
+
+  //if index is more than 80, returns null
+  //returns index value of square nearest to 0 when game is won
+
+
+  function getFirstPlayerTurn() {
+    return boardSettings.colors.p1;
+  }
+  //first player to go is player 1 (red)
+
+  function restartGame() {
+    setCurrentPlayer(getFirstPlayerTurn());
+    setWin(null);
+    setBoard(createBoard());
+  }
+
+  //when game is restarted, the current player (in state), is the first player to go(calling that function)
+  //When game is restarted, no winner
+  //When game is restarted, sets board to the new empty array
+
+
+
+  function getDomBoardCell(index) {
+    if (!domBoard.current) return;
+    const board = domBoard.current;
+    const blocks = board.querySelectorAll(".board-block");
+    return blocks[index];
+  }
+
+//If domBoard doesn't exist, return null
+//If domBoard does exist:
+  //set board to domBoard
+  //set blocks to all the board tiles that have ".board-block" associated with it (80 tiles)
+  //blocks[index] gives index value for each block since blocks calls all blocks
+
+  return(
+    <div>
+    </div>
   )
 }
